44805831e08591ec146dfe46d8e89ec972991d50
marcos <mmunoz2@cendrassos.net>
2024-04-18 18:08:48 +0200

    a

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Laravel\Socialite\Facades\Socialite;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FriendsController;
 
 use App\Models\User;
 
@@ -45,4 +46,5 @@ Route::get('/auth/google/callback', function () {
 
 require __DIR__ . '/auth.php';
 Route::get('friends', [IndexController::class, 'show'])->name('friends.show');
+Route::get('friends', [FriendsController::class, 'index'])->name('friends.index');
 require __DIR__.'/auth.php';
