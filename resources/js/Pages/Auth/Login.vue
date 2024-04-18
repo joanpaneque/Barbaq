83313c7adfdca427e25608aa2aac7c3166c2a8a4
Aniol Moreno Batlle <amoreno@cendrassos.net>
2024-04-18 18:08:57 +0200

    Login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -27,13 +27,17 @@ const submit = () => {
         onFinish: () => form.reset('password'),
     });
 };
+
+const googleLogin = () => {
+    window.location.href = '/auth/google';
+};
 </script>
 
 <template>
-    <GuestLayout>
+
         <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <!-- <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
 
@@ -89,6 +93,203 @@ const submit = () => {
                     Log in
                 </PrimaryButton>
             </div>
-        </form>
-    </GuestLayout>
+        </form> -->
+
+        
+        <div class="center">
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
+            </div>
+
+            <form @submit.prevent="submit" class="form">
+                <div class="flex-column">
+                    <label>Email</label>
+                </div>
+                <div class="inputForm">
+                    <img src="/assets/img/arrova.png" alt="Arrova" class="w-5">
+                    <input
+                        id="email"
+                        type="email"
+                        class="input" 
+                        placeholder="Escriu el teu Email" 
+                        v-model="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                    >
+                </div>
+
+                <div class="flex-column">
+                    <label>Contrasenya</label>
+                </div>
+                <div class="inputForm">
+                    <img src="/assets/img/lock.png" alt="Lock" class="w-5">
+                    <input 
+                        id="password"
+                        type="password"
+                        class="input"
+                        placeholder="Escriu la teva Contrasenya"
+                        v-model="form.password"
+                        required
+                        autocomplete="current-password"
+                    >
+                </div>
+
+                <div class="flex-row">
+                    <div>
+                        <input type="radio" v-model="form.remember">
+                        <label>Recorda'm</label>
+                    </div>
+                    <!-- <span class="span">Has oblidat la contrasenya?</span> -->
+                </div>
+
+                <button class="button-submit" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">Iniciar Sessió</button>
+                
+                <p class="p">
+                    No tens compte? <Link :href="route('register')" class="span">Registra't</Link>
+                </p>
+
+                <p class="p line">O amb</p>
+
+                <div class="flex-row">
+                    <button 
+                        class="btn google"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                        @click="googleLogin"
+                    >
+                        <img src="/assets/svg/google_icon.svg" alt="Google" class="w-8">
+                        Google
+                    </button>
+                </div>
+            </form>
+        </div>
+
 </template>
+
+<style>
+body {
+    background-color: #E4E4E4;
+}
+
+.center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background-color: #ffffff;
+    padding: 30px;
+    width: 450px;
+    border-radius: 20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+::placeholder {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.form button {
+    align-self: flex-end;
+}
+
+.flex-column>label {
+    color: #151717;
+    font-weight: 600;
+}
+
+.inputForm {
+    border: 1.5px solid #ecedec;
+    border-radius: 10px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    transition: 0.2s ease-in-out;
+}
+
+.input {
+    border-radius: 10px;
+    border: none;
+    width: 100%;
+    height: 100%;
+}
+
+.input:focus {
+    outline: none;
+    --tw-ring-shadow: none !important;
+}
+
+.inputForm:focus-within {
+    border: 1.5px solid #2d79f3;
+}
+
+.flex-row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+    justify-content: space-between;
+}
+
+.flex-row > div > label {
+    font-size: 14px;
+    color: black;
+    font-weight: 400;
+    padding-left: 10px;
+}
+
+.span {
+    font-size: 14px;
+    margin-left: 5px;
+    color: #2d79f3;
+    font-weight: 500;
+    cursor: pointer;
+}
+
+.button-submit {
+    margin: 20px 0 10px 0;
+    background-color: #151717;
+    border: none;
+    color: white;
+    font-size: 15px;
+    font-weight: 500;
+    border-radius: 10px;
+    height: 50px;
+    width: 100%;
+    cursor: pointer;
+}
+
+.p {
+    text-align: center;
+    color: black;
+    font-size: 14px;
+    margin: 5px 0;
+}
+
+.btn {
+    margin-top: 10px;
+    width: 100%;
+    height: 50px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+    gap: 10px;
+    border: 1px solid #ededef;
+    background-color: white;
+    cursor: pointer;
+    transition: 0.2s ease-in-out;
+}
+
+.btn:hover {
+    border: 1px solid #2d79f3;
+    ;
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
+
+use App\Models\User;
 
 Route::get('/', [IndexController::class, 'show'])->name('index');
 
@@ -13,4 +17,30 @@ Route::middleware('auth')->group(function () {
     Route::resource('profile', ProfileController::class)->only(['edit', 'update', 'destroy']);
 });
 
-require __DIR__.'/auth.php';
+
+// Google login
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+
+    $user = User::where('email', $googleUser->email)->first();
+
+    if (!$user) {
+        $user = User::create([
+            'id' => $googleUser->id,
+            'name' => $googleUser->user['given_name'],
+            'email' => $googleUser->email,
+            'password' => Str::random(16),
+        ]);
+    }
+
+    // Log the user in
+    Auth::login($user);
+
+    return redirect('/');
+});
+
+
+require __DIR__ . '/auth.php';
