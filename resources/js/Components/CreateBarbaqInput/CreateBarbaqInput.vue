e0762bd09c67f6820908b5e0f4ae3dddec287662
Roman <rmysyura@cendrassos.net>
2024-04-19 18:42:40 +0200

    Form create bbq added

--- resources/js/Components/CreateBarbaqInput/CreateBarbaqInput.vue
+++ resources/js/Components/CreateBarbaqInput/CreateBarbaqInput.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { createApp } from 'vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+
+const app = createApp()
+app.component('QuillEditor', QuillEditor)
 
 const isOpen = ref(false);
 
@@ -8,49 +15,122 @@ const toggleDropdown = () => {
         isOpen.value = true;
     }
 };
+
 </script>
 
 <template>
     <div class="dropdown" :class="{ open: isOpen }" @click="toggleDropdown">
 
         <div class="flex">
-            <form action="" class="w-full">
-                <input class="w-full" type="text" placeholder="Nova barbacoa...">
+            <img src="/assets/img/profile.jpg" alt="Foto de perfil" class="mr-2 h-16 w-16 rounded-full">
+            <form action="" class="w-full mt-auto mb-auto ">
+                <div class="input-container">
+                    <input type="text" placeholder="Crear nova barbacoa...">
+                </div>
             </form>
-            <svg width="26px" height="26px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                <g id="SVGRepo_iconCarrier">
-                    <path
-                        d="M20.1498 7.93997L8.27978 19.81C7.21978 20.88 4.04977 21.3699 3.32977 20.6599C2.60977 19.9499 3.11978 16.78 4.17978 15.71L16.0498 3.84C16.5979 3.31801 17.3283 3.03097 18.0851 3.04019C18.842 3.04942 19.5652 3.35418 20.1004 3.88938C20.6356 4.42457 20.9403 5.14781 20.9496 5.90463C20.9588 6.66146 20.6718 7.39189 20.1498 7.93997V7.93997Z"
-                        stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                </g>
-            </svg>
+            <img src="/assets/svg/edit.svg" alt="Imatge de editar" class="">
         </div>
 
         <div class="content" :style="{ height: isOpen ? 'auto' : '0' }">
-            <h1>sdfsd</h1>
-            <h1>sdfsd</h1>
-            <h1>sdfsd</h1>
-            <h1>sdfsd</h1>
-            <h1>sdfsd</h1>
-            
+
+
+            <section class="mt-3">
+
+                <QuillEditor theme="snow" class="rounded-b-lg" />
+
+            </section>
+
+            <editor-content :editor="editor" class="editor-content" />
+            <div class="flex">
+                <button class=""><img src="/assets/svg/addphoto.svg" alt="Imatge de editar" class=""></button>
+
+
+                <button class="boton-elegante ml-auto mt-2">Publicar</button>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .dropdown {
-    position: relative;
-    width: 100%;
-    background-color: white;
-    border-radius: 20px;
-    padding: 20px;
-    cursor: pointer;
+  position: relative;
+  width: 100%;
+  background-color: white;
+  border-radius: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
 }
 
 .content {
+  overflow: hidden;
+  transition: max-height 0.3s ease-out;
+  max-height: 0; /* Inicialmente oculto */
+}
+
+.dropdown.open .content {
+  max-height: 300px; /* Altura máxima del contenido */
+}
+::placeholder {
+    color: #a8a8a8;
+}
+
+.boton-elegante {
+    padding: 5px 20px;
+    width: 250px;
+    border: 2px solid #2c2c2c;
+    background-color: #1a1a1a;
+    color: #ffffff;
+    font-size: 1.2rem;
+    cursor: pointer;
+    border-radius: 20px;
+    transition: all 0.4s ease;
+    outline: none;
+    position: relative;
     overflow: hidden;
-    transition: height 0.3s ease;
+    font-weight: bold;
+}
+
+.boton-elegante::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(circle,
+            rgba(255, 255, 255, 0.25) 0%,
+            rgba(255, 255, 255, 0) 70%);
+    transform: scale(0);
+    transition: transform 0.5s ease;
+}
+
+.boton-elegante:hover::after {
+    transform: scale(4);
+}
+
+.boton-elegante:hover {
+    border-color: #666666;
+    background: #292929;
+}
+
+
+
+.input-container {
+    position: relative;
+
+}
+
+.input-container input {
+    width: 100%;
+    font-size: 2rem;
+    border: none;
+    border-bottom: 2px solid transparent;
+    /* Línea inicialmente transparente */
+    outline: none;
+}
+
+.input-container input:focus {
+    border-bottom-color: gray;
+    /* Color de la línea cuando el input está enfocado */
 }
 </style>
