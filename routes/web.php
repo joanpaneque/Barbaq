f9a2289cee345a0459570942b93589b430e743a2
ecardosa <ecardosa@cendrassos.net>
2024-04-19 16:28:29 +0200

    chat page

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,14 @@ use Laravel\Socialite\Facades\Socialite;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BarbecuesController;
+use App\Http\Controllers\TestController;
 
 use App\Models\User;
 
 Route::get('/', [IndexController::class, 'show'])->name('index');
+
+Route::get('/test', [TestController::class, 'index'])->name('test');
 
 Route::middleware('auth')->group(function () {
     Route::resource('profile', ProfileController::class)->only(['edit', 'update', 'destroy']);
