773b3bdcef90fb865097b992430bb8ee34aa2135
Aniol Moreno Batlle <amoreno@cendrassos.net>
2024-04-19 12:56:18 +0200

    Div barbacoa

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,12 @@
 <script setup>
-    import MainLayout from "@/Layouts/MainLayout.vue";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import DivBarbacoa from "@/Components/Index/DadesBarbacoa.vue";
 </script>
 
 <template>
     <MainLayout title="Inici">
         <template #main-content>
-            
+            <DivBarbacoa />
         </template>
         <template #right-aside>
             <div class="aside-menu">
