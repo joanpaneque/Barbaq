75ae53afb0f8064ba0c07596b0cd1ee4de16b534
ecardosa <ecardosa@cendrassos.net>
2024-04-24 15:01:00 +0200

    editar perfil

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -3,7 +3,14 @@ import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 import { Head } from '@inertiajs/vue3';
+import ProfileLayout from '@/Layouts/ProfileLayout.vue';
 
+import Banner from "@/Components/UserProfile/Banner.vue";
+import BarbaqUserProfile from "@/Components/UserProfile/BarbaqUserProfile.vue";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+authStore.updateUserData();
 defineProps({
     mustVerifyEmail: {
         type: Boolean,
@@ -15,7 +22,30 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Profile" />
+    <ProfileLayout title="Inici">
+        <template #main-content>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 overflow">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <UpdateProfileInformationForm
+                        :must-verify-email="mustVerifyEmail"
+                        :status="status"
+                        class="max-w-xl"
+                    />
+                </div>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <UpdatePasswordForm class="max-w-xl" />
+                </div>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <DeleteUserForm class="max-w-xl" />
+                </div>
+            </div>
+        </template>
+    </ProfileLayout>
+</template>
+
+
+
+<!-- <Head title="Profile" />
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
@@ -34,5 +64,5 @@ defineProps({
                     <DeleteUserForm class="max-w-xl" />
                 </div>
             </div>
-        </div>
-</template>
+        </div> -->
+
