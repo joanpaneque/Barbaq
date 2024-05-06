26a24e78088e552779f2c63ac0032a41435ca7e3
ecardosa <ecardosa@cendrassos.net>
2024-05-06 16:15:06 +0200

    send invitation

--- resources/js/Components/Barbecues/BarbacuesAside.vue
+++ resources/js/Components/Barbecues/BarbacuesAside.vue
@@ -47,8 +47,8 @@ const form = useForm({
 });
 
 const inviteUser = (friendId) => {
-form.friend_id = friendId;
-form.post('/sendinvitation/' + barbecue.id);
+    form.friend_id = friendId;
+    form.post('/sendinvitation/' + barbecue.id);
 
 }
 
@@ -209,12 +209,22 @@ form.post('/sendinvitation/' + barbecue.id);
 
                         <img :src="friend.image" alt="" class="fit-content h-10 w-10 rounded-full object-cover">
                         <p>{{ friend.name }}</p>
+                        <div class="flex justify-end w-full">
+                            <Link @click="inviteUser(friend.id)" class="flex items-center pr-1">
+                            <button title="Add New"
+                                class="group cursor-pointer outline-none hover:rotate-90 duration-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24"
+                                    class="stroke-pink-400 fill-none group-hover:fill-pink-800 group-active:stroke-pink-200 group-active:fill-pink-600 group-active:duration-0 duration-300">
+                                    <path
+                                        d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                                        stroke-width="1.5"></path>
+                                    <path d="M8 12H16" stroke-width="1.5"></path>
+                                    <path d="M12 16V8" stroke-width="1.5"></path>
+                                </svg>
+                            </button>
 
-                        <Link 
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                        @click="inviteUser(friend.id)">
-                            Invitar
-                        </Link>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
