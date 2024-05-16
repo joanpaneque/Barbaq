ac11106df361401de33ea4bb6258d8dc34591d5e
marcos <mmunoz2@cendrassos.net>
2024-05-16 15:11:36 +0200

    EditBarbecues

--- resources/js/Pages/Barbecues/Edit.vue
+++ resources/js/Pages/Barbecues/Edit.vue
@@ -18,7 +18,6 @@ const props = defineProps({
         required: true
     }
 });
-
 const barbecueStore = useBarbecueStore();
 barbecueStore.setBarbecue(props.barbecue);
 console.log(barbecueStore.barbecue);
@@ -43,87 +42,64 @@ const submitForm = () => {
 </script>
 
 <template>
-   <MainLayout :title="barbecue.title">
-    <template #main-content>
-        <div class="flex justify-center items-center h-screen">
-            <div class="bg-white rounded-lg p-8 w-full md:max-w-xl">
-                <form @submit.prevent="submitForm" class="mt-6 space-y-6">
-                    <div class="flex flex-col">
-                        <div class="mb-6">
-                            <InputLabel for="title" value="Titol" />
-                            <TextInput
-                                id="title"
-                                type="text"
-                                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                v-model="form.title"
-                                required
-                                autofocus
-                            />
-                            <InputError class="mt-2" :message="form.errors.title" />
+    <MainLayout :title="barbecue.title">
+        <template #main-content>
+            <div class="flex justify-center items-center h-screen">
+                <div class="bg-white rounded-lg p-8 w-full md:max-w-xl">
+                    <form @submit.prevent="submitForm" class="mt-6 space-y-6">
+                        <div class="flex flex-col">
+                            <div class="mb-6">
+                                <InputLabel for="title" value="Titol" />
+                                <TextInput id="title" type="text"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                    v-model="form.title" required autofocus />
+                                <InputError class="mt-2" :message="form.errors.title" />
+                            </div>
+                            <div class="mb-6">
+                                <InputLabel for="content" value="Descripció" />
+                                <div id="content" type="text"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                    v-html="form.content" required></div>
+                                <InputError class="mt-2" :message="form.errors.content" />
+                            </div>
+                            <div class="mb-6">
+                                <InputLabel for="address" value="Adreça" />
+                                <TextInput id="address" type="text"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                    v-model="form.address" required />
+                                <InputError class="mt-2" :message="form.errors.address" />
+                            </div>
+                            <div class="mb-6">
+                                <InputLabel for="date" value="Data" />
+                                <input id="date" type="datetime-local"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                    v-model="form.date" required />
+                                <InputError class="mt-2" :message="form.errors.date" />
+                            </div>
                         </div>
-                        <div class="mb-6">
-                            <InputLabel for="content" value="Descripció" />
-                            <div
-                                id="content"
-                                type="text"
-                                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                v-html="form.content"
-                                required
-                            ></div>
-                            <InputError class="mt-2" :message="form.errors.content" />
-                        </div>
-                        <div class="mb-6">
-                            <InputLabel for="address" value="Adreça" />
-                            <TextInput
-                                id="address"
-                                type="text"
-                                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                v-model="form.address"
-                                required
-                            />
-                            <InputError class="mt-2" :message="form.errors.address" />
-                        </div>
-                        <div class="mb-6">
-                            <InputLabel for="text" value="Data" />
-                            <input
-                                id="date"
-                                type="text"
-                                class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                v-model="form.date"
-                                required
-                            />
-                            <InputError class="mt-2" :message="form.errors.date" />
-                        </div>
-                    </div>
 
-                    <div class="flex items-center justify-end">
-                        <button 
-                            type="submit"
-                            class="btn hover:bg-[#c84c00] hover:text-white bg-[#FF6100] text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FF6100] focus:ring-offset-white active:bg-[#FF6100] active:text-white px-4 py-2 rounded-md transition ease-in-out duration-150"
-                            :disabled="form.processing"
-                        >
-                            Guardar
-                        </button>
+                        <div class="flex items-center justify-end">
+                            <button type="submit"
+                                class="btn hover:bg-[#c84c00] hover:text-white bg-[#FF6100] text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FF6100] focus:ring-offset-white active:bg-[#FF6100] active:text-white px-4 py-2 rounded-md transition ease-in-out duration-150"
+                                :disabled="form.processing">
+                                Guardar
+                            </button>
 
-                        <Transition
-                            enter-active-class="transition ease-in-out"
-                            enter-from-class="opacity-0"
-                            leave-active-class="transition ease-in-out"
-                            leave-to-class="opacity-0"
-                        >
-                            <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Guardat.</p>
-                        </Transition>
-                    </div>
-                </form>
+                            <Transition enter-active-class="transition ease-in-out" enter-from-class="opacity-0"
+                                leave-active-class="transition ease-in-out" leave-to-class="opacity-0">
+                                <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Guardat.</p>
+                            </Transition>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    </template>
-    <template #right-aside>
-        <div class="aside-menu">
-            <BarbacuesAside/>
-        </div>
-    </template>
-</MainLayout>
+        </template>
+        <template #right-aside>
+            <div class="aside-menu">
+                <BarbacuesAside />
+            </div>
+        </template>
+    </MainLayout>
 </template>
 
 <style scoped>
@@ -133,4 +109,5 @@ const submitForm = () => {
     background: white;
     border-radius: 20px;
 }
+
 </style>
