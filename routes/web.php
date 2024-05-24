e24faf5cd91d8e1f1822f5a6b9fa684ec0537583
Roman <rmysyura@cendrassos.net>
2024-05-24 16:10:50 +0200

    MIddleware + Bugs

--- routes/web.php
+++ routes/web.php
@@ -20,82 +20,66 @@ use App\Http\Controllers\ParticipationController;
 
 
 
-Route::delete('/friendship-destroy', [BarbecuesController::class, 'destroyFriendship'])->name('destroyfriendship');
 
 Route::middleware('auth')->group(function () {
 
-Route::get('/', [IndexController::class, 'show'])->name('index');
-
-    Route::resource('profile', ProfileController::class)->only(['edit', 'update', 'destroy']);
-});
-
-Route::get('/test', [TestController::class, 'index'])->name('test');
-Route::get('/test/profile', [TestController::class, 'indexProfile'])->name('test.profile');
-
-
-Route::post('/api/chat/{barbecueId}', [ChatMessagesController::class, 'store']);
-
-
-Route::resource('profile', ProfileController::class);
-Route::resource('participations', ParticipationController::class);
-
-Route::get('/api/my-barbecues', [BarbecuesController::class, 'apiMyBarbecues']);
-// Route::resource('/barbecues', BarbecuesController::class);
-
-
-
-Route::get('friends', [FriendsController::class, 'index'])->name('friends.index');
-
-// Route::get('/messages', [MessagesController::class, 'index'])->name('messages.index');
-
-Route::get('/auth/google', [GoogleController::class, 'index']);
-Route::get('/auth/google/callback', [GoogleController::class, 'store']);
-
-Route::middleware('auth')->group(function () {
+    Route::get('/', [IndexController::class, 'show'])->name('index');
     Route::resource('profile', ProfileController::class);
+    Route::delete('/friendship-destroy', [BarbecuesController::class, 'destroyFriendship'])->name('destroyfriendship');
+    
+    Route::get('/test', [TestController::class, 'index'])->name('test');
+    Route::get('/test/profile', [TestController::class, 'indexProfile'])->name('test.profile');
+
+
+    Route::post('/api/chat/{barbecueId}', [ChatMessagesController::class, 'store']);
+
+    Route::resource('participations', ParticipationController::class);
+
+    Route::get('/api/my-barbecues', [BarbecuesController::class, 'apiMyBarbecues']);    
+
+    Route::get('friends', [FriendsController::class, 'index'])->name('friends.index');
+
+    Route::get('/auth/google', [GoogleController::class, 'index']);
+    Route::get('/auth/google/callback', [GoogleController::class, 'store']);
+
+    Route::delete('/friends/{id}', [FriendsController::class, 'destroy'])->name('friends.destroy');
+
+    Route::post('/sendfriendrequest/{id}', [FriendsController::class, 'store'])->name('sendfriendrequest');
+
+    Route::get('/api/user', [UserController::class, 'apiShowLogged']);
+
+    Route::post('/updateuserphoto/{id}', [UserController::class, 'update'])->name('updateuserphoto');
+
+    Route::resource('barbecues', BarbecuesController::class);
+    Route::resource('/barbecues/{barbecueId}/images', ImagesController::class);
+    Route::get('/api/barbecues', [BarbecuesController::class, 'apiIndex']);
+
+    Route::post('/api/discordbot', [DiscordBotController::class, 'broadcast']);
+
+    Route::post('/updateuserdescription/{id}', [UserController::class, 'updateDescription'])->name('updateuserdescription');
+
+    Route::post('/sendinvitation/{id}', [BarbecuesController::class, 'sendInvitation'])->name('sendinvitation');
+
+    Route::post('/sendbarbecuejoinrequest/{id}', [BarbecuesController::class, 'sendBarbecueJoinRequest'])->name('sendbarbecuejoinrequest');
+
+    Route::post('/acceptbarbecuejoinrequest/{barbecueId}/{userId}', [BarbecuesController::class, 'acceptBarbecueJoinRequest'])->name('acceptbarbecuejoinrequest');
+
+    Route::post('/toggleprivate', [UserController::class, 'togglePrivate'])->name('toggleprivate');
+
+    Route::post('/addproduct/{id}', [BarbecuesController::class, 'addProduct'])->name('addproduct');
+    Route::post('/minusproduct/{id}', [BarbecuesController::class, 'minusProduct'])->name('minusproduct');
+    Route::post('/assignproduct/{id}', [BarbecuesController::class, 'assignProduct'])->name('assignproduct');
+
+
+    Route::resource('notifications', NotificationsController::class);
+    Route::resource('comments', CommentsController::class);
+
 });
 
-
-
+// Route::resource('/barbecues', BarbecuesController::class);
+// Route::get('/messages', [MessagesController::class, 'index'])->name('messages.index');
 // Route::resource('/barbecues/{barbecueId}/edit', BarbecuesController::class);
-
 // Route::get('/api/barbecue/{id}/messages', [MessagesController::class, 'apiIndex']);
-
-Route::delete('/friends/{id}', [FriendsController::class, 'destroy'])->name('friends.destroy');
-
-Route::post('/sendfriendrequest/{id}', [FriendsController::class, 'store'])->name('sendfriendrequest');
-
-Route::get('/api/user', [UserController::class, 'apiShowLogged']);
-
-Route::post('/updateuserphoto/{id}', [UserController::class, 'update'])->name('updateuserphoto');
-
-Route::resource('barbecues', BarbecuesController::class);
-Route::resource('/barbecues/{barbecueId}/images', ImagesController::class);
-Route::get('/api/barbecues', [BarbecuesController::class, 'apiIndex']);
-
-
-
-Route::post('/api/discordbot', [DiscordBotController::class, 'broadcast']);
-
-Route::post('/updateuserdescription/{id}', [UserController::class, 'updateDescription'])->name('updateuserdescription');
-
-Route::post('/sendinvitation/{id}', [BarbecuesController::class, 'sendInvitation'])->name('sendinvitation');
-
-Route::post('/sendbarbecuejoinrequest/{id}', [BarbecuesController::class, 'sendBarbecueJoinRequest'])->name('sendbarbecuejoinrequest');
-
-Route::post('/acceptbarbecuejoinrequest/{barbecueId}/{userId}', [BarbecuesController::class, 'acceptBarbecueJoinRequest'])->name('acceptbarbecuejoinrequest');
-
-Route::post('/toggleprivate', [UserController::class, 'togglePrivate'])->name('toggleprivate');
-
-Route::post('/addproduct/{id}', [BarbecuesController::class, 'addProduct'])->name('addproduct');
-Route::post('/minusproduct/{id}', [BarbecuesController::class, 'minusProduct'])->name('minusproduct');
-Route::post('/assignproduct/{id}', [BarbecuesController::class, 'assignProduct'])->name('assignproduct');
-
-
-Route::resource('notifications', NotificationsController::class);
-Route::resource('comments', CommentsController::class);
 
 
 require __DIR__ . '/auth.php';
-
-
