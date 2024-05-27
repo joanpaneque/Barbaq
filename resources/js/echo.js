88259b07e19336fb781dde1c95a189e6e54219da
Joan Paneque Domingo <joanpd0@gmail.com>
2024-05-27 18:46:31 +0200

    websockets

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,7 +6,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY ?? '3ngneno2usudnbxo0h1y',
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? 'https://barbaq.es',
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? 'barbaq.es',
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
