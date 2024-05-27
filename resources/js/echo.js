33cae08a5a8658dff861b03866be784c1c49fda1
Joan Paneque Domingo <joanpd0@gmail.com>
2024-05-27 18:05:28 +0200

    Prof test

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -12,13 +12,21 @@ console.log({
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+},{
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY ?? '3ngneno2usudnbxo0h1y',
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? 'https://barbaq.es',
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 })
 
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    key: import.meta.env.VITE_REVERB_APP_KEY ?? '3ngneno2usudnbxo0h1y',
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? 'https://barbaq.es',
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
